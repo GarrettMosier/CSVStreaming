d9f49b7a941fbf3c85e5173229cd26501771e244
Garrett Mosier <x3g2@nordstrom.com>
2015-08-16 11:00:25 -0700

    Added posibility of null with Nothing

--- Main.hs
+++ Main.hs
@@ -2,16 +2,26 @@
 -- Dynamically generate record from generator exe
 data Stats = Textual Int Int Double Double Double | Numeric Int Int Int Int Double deriving Show
 
-updateStats :: Stats -> [Char] -> Stats
-updateStats oldStats@(Textual count nullCount shortCount longCount averageLen) msg = 
-    (Textual (count + 1) (nullCount + 1) (shortCount + 1) (longCount + 1) (updateAverage count averageLen (length msg))) 
-updateStats oldStats@(Numeric count nullCount min max average) msg = oldStats
+updateColumnStatsUnsafe :: Stats -> String -> Stats
+updateColumnStatsUnsafe oldStats@(Textual count nullCount shortCount longCount averageLen) msg = 
+    (Textual (count + 1) nullCount (shortCount + 1) (longCount + 1) (updateAverage count averageLen (length msg))) 
+updateColumnStatsUnsafe oldStats@(Numeric count nullCount min max average) msg = oldStats
+
+updateColumnStatsSafe :: Stats -> Maybe String -> Stats 
+updateColumnStatsSafe oldStats@(Textual count nullCount shortCount longCount averageLen) Nothing = 
+    (Textual count (nullCount + 1) shortCount longCount averageLen) 
+updateColumnStatsSafe oldStats@(Numeric count nullCount min max average) Nothing = oldStats
+updateColumnStatsSafe oldStats (Just val) = updateColumnStatsUnsafe oldStats val
 
 updateAverage :: Int -> Double -> Int -> Double 
 updateAverage count oldAverage updateVal = (oldAverage * fromIntegral count + fromIntegral updateVal) / fromIntegral (count + 1)
 
+updateStats :: [Stats] -> [Maybe String] -> [Stats]
+updateStats oldStats columnValues = zipWith updateColumnStatsSafe oldStats columnValues
+
 main = do
-    let testText = (Textual 0 0 0 0 0)
-    let testNumeric = (Numeric 0 0 0 0 0)
-    let messages = ["TEST", "DSA", "2d"]
-    print $ foldl updateStats testText messages 
+    -- One stat for each column
+    let testStats = [(Textual 0 0 0 0 0), (Numeric 0 0 0 0 0)]
+    -- One message for each column
+    let messages = [[Just "TEST", Just "DSA", Just "2d", Nothing]]
+    print $ foldl updateStats testStats messages 
