5c3d90d88cfef4465fb13231821a3f07ad46f7cf
Garrett Mosier <x3g2@nordstrom.com>
2015-08-16 08:55:38 -0700

    Created text and num stats data types

--- Main.hs
+++ Main.hs
@@ -1,2 +1,14 @@
+-- Use record instead
+-- Dynamically generate record from generator exe
+data Stats = Textual Int Int Double Double Double | Numeric Int Int Int Int Double deriving Show
+
+updateStats :: Stats -> [Char] -> Stats
+updateStats oldStats@(Textual a b c d e) msg = oldStats
+updateStats oldStats@(Numeric a b c d e) msg = oldStats
+
 main = do
     print "TEST"
+    let testText = (Textual 0 0 0 0 0)
+    let testNumeric = (Numeric 0 0 0 0 0)
+    print $ updateStats testText "DSA"
+    print $ updateStats testNumeric "DSA"
