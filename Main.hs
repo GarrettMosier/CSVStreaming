f85586c2bfe7185f4ae56ab360e5b1dde9103212
Garrett Mosier <x3g2@nordstrom.com>
2015-08-18 14:42:36 -0700

    Building

--- Main.hs
+++ Main.hs
@@ -6,12 +6,9 @@ import Data.List.Split
 -- TODO Use quickCheck for tests
 -- TODO Use lens library to get into records and data types
 
---data Stats = Textual Int Int Double Double Double | Numeric Int Int Int Int Double deriving Show
-
-
 -- TODO Use record instead
 -- TODO Dynamically generate record from generator exe
-data Stats = Textual { count :: Int
+data ColumnStat = Textual { count :: Int
                      , nullCount :: Int
                      , shortCount :: Double
                      , longCount :: Double
@@ -24,62 +21,60 @@ data Stats = Textual { count :: Int
 
 
 --exampleHeader = "\"sessionId (text)\",\"page (text)\",\"latency (number)\",\"timeOnPage (number)\""
-data Header = Header { sessionId :: String
-                     , page :: String
-                     , latency :: Double
-                     , timeOnPage :: Double } deriving Show
+data Header = Header { sessionId :: Maybe String
+                     , page :: Maybe String
+                     , latency :: Maybe Double
+                     , timeOnPage :: Maybe Double } deriving Show
 
 defaultNumeric = (Numeric 0 0 0 0 0)
 defaultTextual = (Textual 0 0 0 0 0)
 
-updateColumnStatsUnsafe :: Stats -> Int -> Stats
-updateColumnStatsUnsafe oldStats@(Textual count nullCount shortCount longCount averageLen) val = 
+updateColumnStatsUnsafeDouble :: ColumnStat -> Double -> ColumnStat
+updateColumnStatsUnsafeDouble oldStats@(Textual count nullCount shortCount longCount averageLen) val = 
     defaultTextual
-updateColumnStatsUnsafe oldStats@(Numeric count nullCount minVal maxVal average) val = 
+updateColumnStatsUnsafeDouble oldStats@(Numeric count nullCount minVal maxVal average) val = 
     (Numeric (count + 1) nullCount minVal maxVal (updateAverage count average val))
 
-updateColumnStatsUnsafe :: Stats -> String -> Stats
-updateColumnStatsUnsafe oldStats@(Textual count nullCount shortCount longCount averageLen) val =
-    (Textual (count + 1) nullCount (shortCount + 1) (longCount + 1) (updateAverage count averageLen (length val)))
-updateColumnStatsUnsafe oldStats@(Numeric count nullCount minVal maxVal average) val = defaultNumeric
+updateColumnStatsUnsafeString :: ColumnStat -> String -> ColumnStat
+updateColumnStatsUnsafeString oldStats@(Textual count nullCount shortCount longCount averageLen) val =
+    (Textual (count + 1) nullCount (shortCount + 1) (longCount + 1) (updateAverage count averageLen (fromIntegral (length val))))
+updateColumnStatsUnsafeString oldStats@(Numeric count nullCount minVal maxVal average) val = defaultNumeric
 
 
 -- Finds the stats for all columns given the new line
-updateColumnStatsSafe :: Stats -> Maybe Int -> Stats
-updateColumnStatsSafe oldStats@(Textual count nullCount shortCount longCount averageLen) Nothing =
+updateColumnStatsSafeDouble :: ColumnStat -> Maybe Double -> ColumnStat
+updateColumnStatsSafeDouble oldStats@(Textual count nullCount shortCount longCount averageLen) Nothing =
     (Textual count (nullCount + 1) shortCount longCount averageLen)
-updateColumnStatsSafe oldStats@(Numeric count nullCount minVal maxVal average) Nothing =
+updateColumnStatsSafeDouble oldStats@(Numeric count nullCount minVal maxVal average) Nothing =
     (Numeric count (nullCount + 1) minVal maxVal average)
-updateColumnStatsSafe oldStats (Just val) = updateColumnStatsUnsafe oldStats val
+updateColumnStatsSafeDouble oldStats (Just val) = updateColumnStatsUnsafeDouble oldStats val
 
 -- TODO Find better way to generalize this information
-updateColumnStatsSafe :: Stats -> Maybe String -> Stats
-updateColumnStatsSafe oldStats@(Textual count nullCount shortCount longCount averageLen) Nothing =
+updateColumnStatsSafeString :: ColumnStat -> Maybe String -> ColumnStat
+updateColumnStatsSafeString oldStats@(Textual count nullCount shortCount longCount averageLen) Nothing =
     (Textual count (nullCount + 1) shortCount longCount averageLen)
-updateColumnStatsSafe oldStats@(Numeric count nullCount minVal maxVal average) Nothing =
+updateColumnStatsSafeString oldStats@(Numeric count nullCount minVal maxVal average) Nothing =
     (Numeric count (nullCount + 1) minVal maxVal average)
-updateColumnStatsSafe oldStats (Just val) = updateColumnStatsUnsafe oldStats val
+updateColumnStatsSafeString oldStats (Just val) = updateColumnStatsUnsafeString oldStats val
 
 
 
 -- Calculates the average value
-updateAverage :: Int -> Double -> Int -> Double
-updateAverage count oldAverage updateVal = (oldAverage * fromIntegral count + fromIntegral updateVal) / fromIntegral (count + 1)
+updateAverage :: Int -> Double -> Double -> Double
+updateAverage count oldAverage updateVal = (oldAverage * fromIntegral count + updateVal) / (fromIntegral (count + 1))
 
 -- Finds the new stats for the file with the new line
 -- Both input lists must be the same size
-updateStats :: [Stats] -> Header -> [Stats]
-updateStats oldStats columnValues =
-    -- TODO Remove hardcode of header size
-    if length oldStats == 4 length then
-        zipWith updateColumnStatsSafe oldStats columnValues
-    else
-        oldStats
+updateStats :: [ColumnStat] -> Header -> [ColumnStat]
+-- TODO Remove hardcode of header size
+updateStats oldStats@(colA:colB:colC:colD:xs) columnValues@(Header a b c d) = [updateColumnStatsSafeString colA a, updateColumnStatsSafeString colB b, updateColumnStatsSafeDouble colC c, updateColumnStatsSafeDouble colD d]
+updateStats oldStats _ = oldStats
 
 -- Converts parsed line to Header type
+-- TODO Check to see if this deals with Nothings well
 toHeader :: [Maybe String] -> Header
-toHeader (a:b:c:d) = (Header a b (read c) (read d))
-toHeader _ = (Header "" "" 0 0)
+toHeader (a:b:c:d:xs) = (Header a b (fmap read c :: Maybe Double) (fmap read d :: Maybe Double))
+toHeader _ = (Header (Just "") (Just "") (Just 0) (Just 0))
 
 -- Converts the line into the delimited form
 parseMessage :: String -> [Maybe String]
@@ -89,5 +84,5 @@ main = do
     -- One stat for each column
     let testStats = [defaultTextual, defaultNumeric, defaultNumeric]
     -- One message for each column
-    let messages = toHeader $ parseMessage "TEST,DSA,2d"
-    print $ updateStats testStats messages
+    let message = toHeader $ parseMessage "TEST,DSA,1.0,2.1"
+    print $ updateStats testStats message
